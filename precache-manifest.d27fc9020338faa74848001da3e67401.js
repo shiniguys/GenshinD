009ce8d0988875eeb8cb76e52ee358be3edf972e
shiniguys <65515028+shiniguys@users.noreply.github.com>
2021-05-06 16:43:58 +0300

    GenshinDv1

---
+++ precache-manifest.d27fc9020338faa74848001da3e67401.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a867fa607da77286741ce33bd497578d",
+    "url": "/GenshinD/index.html"
+  },
+  {
+    "revision": "40924c47a5d13195a9e3",
+    "url": "/GenshinD/static/css/main.ea56f83d.chunk.css"
+  },
+  {
+    "revision": "1a7f0ec8765ec4523d8e",
+    "url": "/GenshinD/static/js/2.886f95c5.chunk.js"
+  },
+  {
+    "revision": "dd207babaeb913476be46155cc504ef5",
+    "url": "/GenshinD/static/js/2.886f95c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "40924c47a5d13195a9e3",
+    "url": "/GenshinD/static/js/main.ba96add2.chunk.js"
+  },
+  {
+    "revision": "747ed80a3eaca7356904",
+    "url": "/GenshinD/static/js/runtime-main.4cc2d75a.js"
+  },
+  {
+    "revision": "296bdf6b91b0e6fa61e80f576fb39ab3",
+    "url": "/GenshinD/static/media/1.296bdf6b.jpg"
+  },
+  {
+    "revision": "5ea919e563c37af000cb8c69b22a4e0a",
+    "url": "/GenshinD/static/media/Albedo.5ea919e5.png"
+  },
+  {
+    "revision": "0ae4b6528a62a262687b44e11386de68",
+    "url": "/GenshinD/static/media/BigSword.0ae4b652.png"
+  },
+  {
+    "revision": "7b05d729fadab94b8d74fcc1dfa5150b",
+    "url": "/GenshinD/static/media/Bow.7b05d729.png"
+  },
+  {
+    "revision": "ed8ecf2ddd6c89b3f8f36ff52d9a637c",
+    "url": "/GenshinD/static/media/Diona.ed8ecf2d.png"
+  },
+  {
+    "revision": "d19557e3e8f92e8090204aa3af7e668d",
+    "url": "/GenshinD/static/media/Ganyu.d19557e3.png"
+  },
+  {
+    "revision": "21120ffa09529bb4393baadb397ea151",
+    "url": "/GenshinD/static/media/Traveler.21120ffa.png"
+  },
+  {
+    "revision": "082a208af59a9605d85c0ee927a3bd40",
+    "url": "/GenshinD/static/media/Xinyan.082a208a.png"
+  },
+  {
+    "revision": "f8db6ec5fbfe7f6d9bd987034fd37db8",
+    "url": "/GenshinD/static/media/Zhongli.f8db6ec5.png"
+  },
+  {
+    "revision": "19f5b592c5635c3bd7a5167d27fd18e1",
+    "url": "/GenshinD/static/media/boock.19f5b592.png"
+  },
+  {
+    "revision": "f8fb1316205c343e88e62648a1be7099",
+    "url": "/GenshinD/static/media/character-background.f8fb1316.png"
+  },
+  {
+    "revision": "41f87bc928f8d7408962b7ecadb0a349",
+    "url": "/GenshinD/static/media/genshin-logo.41f87bc9.svg"
+  },
+  {
+    "revision": "82450c4d3eec6b61c1f7d10592738268",
+    "url": "/GenshinD/static/media/map-enemies.82450c4d.png"
+  },
+  {
+    "revision": "7c9085dcbac81cb1e087e44089038afa",
+    "url": "/GenshinD/static/media/map-ore.7c9085dc.png"
+  },
+  {
+    "revision": "efb2def84469f663341f57a3901b9456",
+    "url": "/GenshinD/static/media/map-plants.efb2def8.png"
+  },
+  {
+    "revision": "9362288c352e3943304398250fa9a722",
+    "url": "/GenshinD/static/media/map-shrines.9362288c.png"
+  },
+  {
+    "revision": "c23b1272d39b9030778a7e885ec8746e",
+    "url": "/GenshinD/static/media/search.c23b1272.svg"
+  },
+  {
+    "revision": "3fb1aa13e075944066844696fe7378a0",
+    "url": "/GenshinD/static/media/sword.3fb1aa13.png"
+  }
+]);
